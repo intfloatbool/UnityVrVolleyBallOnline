3fa990c53a1cc920824abc94ef3bd7e623736ef1
intfloatbool <intfloatbool@gmail.com>
2020-04-13 11:33:31 +0300

    Online ball spawning , test walls + ball editor base

---
+++ Assets/Scripts/BallOnline.cs
@@ -0,0 +1,28 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace VrVolleyball
+{
+    public class BallOnline : MonoBehaviourPun
+    {
+        [SerializeField] private Rigidbody _rb;
+
+
+        private void Awake()
+        {
+            if(photonView.IsMine == false)
+            {
+                _rb.isKinematic = true;
+            }
+        }
+
+        public void AffectToBall(Vector3 affectVector, float strength)
+        {
+            _rb.AddForce(affectVector * strength);
+        }
+    }
+}
+
---
+++ Assets/Scripts/GameOnlineManager.cs
@@ -0,0 +1,22 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VrVolleyball
+{
+    public class GameOnlineManager : MonoBehaviourPunCallbacks
+    {
+        [SerializeField] private string _ballPrefabName = "ball";
+        [SerializeField] private Vector3 _positionToInstantiate = new Vector3(0, 0.3f,0);
+
+        public override void OnJoinedRoom()
+        {
+            if(PhotonNetwork.IsMasterClient)
+            {
+                PhotonNetwork.Instantiate(_ballPrefabName, _positionToInstantiate, Quaternion.identity);
+            }
+        }
+    }
+
+}
