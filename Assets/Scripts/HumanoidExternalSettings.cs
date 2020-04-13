b68d8be723a39ceff39e522446f5ac2ab37b0213
intfloatbool <intfloatbool@gmail.com>
2020-04-13 10:58:34 +0300

    base

---
+++ Assets/Scripts/HumanoidExternalSettings.cs
@@ -0,0 +1,19 @@
+﻿using Passer;
+using UnityEngine;
+
+namespace VrVolleyball
+{
+    public class HumanoidExternalSettings : MonoBehaviour
+    {
+        [SerializeField] private HumanoidControl _humanoid;
+
+        [Space(5f)]
+        [SerializeField] private bool _isTranslateByPhysics = true;
+
+        private void Awake()
+        {
+            _humanoid.IsTranslateByPhysics = _isTranslateByPhysics;
+        }
+    }
+
+}
